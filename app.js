686c45ac55ef7de34a76ecf35b68402e01ddcd38
Wali Seddiqi <waliseddiqi4@gmail.com>
2021-10-04 10:31:16 +0300

    init

---
+++ app.js
@@ -0,0 +1,30 @@
+const express=require('express')
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+//Test end point
+app.get('/', (req, res) => {
+  res.send("HEeyy");
+});
+
+io.on('connection', (socket)=>{
+ console.log("socket here")
+socket.on(("connected"),(data)=>{
+  socket.broadcast.emit("UserJoined",data);
+  
+})
+///onRecieveMessage
+socket.on(("OnSendMessage"),(data)=>{
+  socket.broadcast.emit("OnRecieveMessage",data);
+})
+
+
+
+
+
+
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
